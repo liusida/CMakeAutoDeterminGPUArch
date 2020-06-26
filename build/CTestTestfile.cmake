68de4a5d147ab2496d4e63b93da845ec94f802ba
sida <sliu1@uvm.edu>
2020-06-26 17:35:13 -0400

    init

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/liusida/code/cmake_6070
-# Build directory: /home/liusida/code/cmake_6070/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
